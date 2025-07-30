ae2ed2e31f802bd614700869a336e054899a4ed4
Mhiztacsong <joh24038@byui.edu>
2025-07-30 02:02:47 +0100

    callbackURL update

--- project2/server.js
+++ project2/server.js
@@ -30,7 +30,7 @@ app.use(passport.session());
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
+  callbackURL: "https://cse341-node-tgkb.onrender.com/auth/github/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const usersCollection = db.collection('users');
